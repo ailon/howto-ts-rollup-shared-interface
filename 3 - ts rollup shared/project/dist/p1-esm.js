39c000cb290249a0bce98794ae404a5c31fc4fb0
Alan Mendelevich <ailon@ailon.org>
2020-01-21 15:49:51 +0200

    fix error by specifying rootDirs
    where's MyInterface.d.ts?

--- 3 - ts rollup shared/project/dist/p1-esm.js
+++ 3 - ts rollup shared/project/dist/p1-esm.js
@@ -1,8 +1,18 @@
+var MySharedClass = /** @class */ (function () {
+    function MySharedClass() {
+    }
+    MySharedClass.prototype.getRandomNumber = function () {
+        return Math.round(Math.random() * 10);
+    };
+    return MySharedClass;
+}());
+
 var MyClass = /** @class */ (function () {
     function MyClass() {
     }
     MyClass.prototype.add2 = function (x) {
-        return x + 2;
+        var randomNumber = new MySharedClass();
+        return x + randomNumber.getRandomNumber() + 2;
     };
     return MyClass;
 }());
